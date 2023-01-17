72556e3c7b9932da3a3b9309f33ce2210b66a3ce
Sacha Harel <sacha.harel@gmail.com>
2023-01-17 16:06:40 +0100

    Update site

--- www/index.php
+++ www/index.php
@@ -1,19 +1,14 @@
-<?php
-
-require_once __DIR__ . '/../src/init.php';
-// $db
-// $_SESSION
-
-$page_title = 'Home page';
-require_once __DIR__ . '/../src/templates/partials/html_head.php';
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CodeBank</title>
+</head>
 <body>
-
-<div>
-    <h1>Home page</h1>
-</div>
-
-<?php require_once __DIR__ . '/../src/templates/partials/footer.php'; ?>
+	<?php
+		include('view/header.php')
+	?>
 </body>
 </html>
